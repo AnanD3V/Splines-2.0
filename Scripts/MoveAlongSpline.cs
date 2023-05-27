c6990ab0fc5b93bf41a9b1804347256110dc6f9c
AnanDEV <107260586+AnanD3V@users.noreply.github.com>
2023-05-27 22:00:17 +0530

    Added Files

---
+++ Scripts/MoveAlongSpline.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class MoveAlongSpline : MonoBehaviour
+{
+    public SplineContainer spline;
+    public float speed = 1f;
+    float distancePercentage = 0f;
+
+    float splineLength;
+
+    private void Start()
+    {
+        splineLength = spline.CalculateLength();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distancePercentage += speed * Time.deltaTime / splineLength;
+
+        Vector3 currentPosition = spline.EvaluatePosition(distancePercentage);
+        transform.position = currentPosition;
+
+        if (distancePercentage > 1f)
+        {
+            distancePercentage = 0f;
+        }
+
+        Vector3 nextPosition = spline.EvaluatePosition(distancePercentage + 0.05f);
+        Vector3 direction = nextPosition - currentPosition;
+        transform.rotation = Quaternion.LookRotation(direction, transform.up);
+    }
+}
---
+++ Scripts/TestScript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class TestScript : MonoBehaviour
+{
+    public SplineContainer spline;
+
+}
